1700ec5a4fec22eff7320aa02419bc5b4a73f1c7
0362777 <0362777@schoolsnet.act.edu.au>
2022-02-24 07:24:48 +1100

    Player Shooting

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,13 +1,11 @@
 extends KinematicBody2D
 
 var movement_speed = 200
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var bulletSource = preload ("res://Bullet/Bullet.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	set_process(true)
 	set_physics_process(true)
 
 func _physics_process(delta):
@@ -17,7 +15,12 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		if position.x < 1250: 
 			move_and_collide(Vector2(movement_speed * delta, 0))
-	
+
+func _process(delta):
+	if Input.is_action_just_pressed("fire"):
+		var bulletInstance = bulletSource.instance()
+		bulletInstance.position - Vector2(position.x, position.y-20)
+		get_tree().get_root().add_child(bulletInstance)
 
 
 
